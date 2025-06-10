a66d53229bb98185c9b0492b7150873a99cbf842
Joepheusschen <joepheusschen@gmail.com>
2025-06-10 09:17:56 +0200

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 apt-get update && apt-get install -y \
-  libffi-dev \
+  libpango-1.0-0 \
+  libpangocairo-1.0-0 \
   libcairo2 \
-  libpango1.0-0 \
   libgdk-pixbuf2.0-0 \
+  libffi-dev \
+  libssl-dev \
   libxml2 \
-  libxslt1.1
+  libxslt1.1 \
+  libjpeg-dev \
+  libpng-dev \
+  fonts-liberation \
+  libxrender1 \
+  libharfbuzz-dev \
+  libgraphite2-dev \
+  libglib2.0-0
